804d24f2333301a2691d981f0cb999bf23e782d1
Anka Wolańska <162566937+AnkaWolanska@users.noreply.github.com>
2024-11-12 23:59:27 +0100

    update division

--- app.py
+++ app.py
@@ -10,12 +10,16 @@ def calculate():
     op = request.args.get('op', type=str)
     arg1 = request.args.get('arg1', type=int)
     arg2 = request.args.get('arg2', type=int)
-    if op == '+':
+    if op == 'sum':
         return f"{arg1} + {arg2} = {arg1+arg2}"
-    if op == '-':
+    if op == 'sub':
         return f"{arg1} - {arg2} = {arg1-arg2}"
-    if op == '*':
+    if op == 'mul':
         return f"{arg1} * {arg2} = {arg1*arg2}"
+    if op == 'div':
+        if arg2 == 0:
+            return "Don't divide by 0"
+        return f"{arg1} / {arg2} = {arg1/arg2}"
     return f"invalid operation: {op}"
 
 if __name__ == '__main__':
