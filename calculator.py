7888242bbb0916c3e2fe9f2d382e5f6ad0f9085d
Anka Wolańska <162566937+AnkaWolanska@users.noreply.github.com>
2024-11-15 22:14:56 +0100

    aligning with pylint

--- calculator.py
+++ calculator.py
@@ -1,16 +1,23 @@
+"""Implementing class Calculator"""
+
 class Calculator:
+    """Perform calculations"""
     def __init__(self, op1: float, op2: float):
-       self.op1 = op1
-       self.op2 = op2
+        self.op1 = op1
+        self.op2 = op2
 
     def sum(self):
+        """Perform sum"""
         return self.op1 + self.op2
 
     def subtract(self):
+        """Perform subtract"""
         return self.op1 - self.op2
 
     def multiply(self):
+        """Perform multiplying"""
         return self.op1 * self.op2
 
     def divide(self):
+        """Perform division"""
         return self.op1 / self.op2
